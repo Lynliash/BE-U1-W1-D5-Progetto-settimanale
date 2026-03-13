198545ebb9b84af3e9c405b0372f81e925153067
Lynliash <francesco.alves1408@gmail.com>
2026-03-13 11:12:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# BE-U1-W1-D5-Progetto settimanale
+
---
+++ src/entities/ElementoMultimediale.java
@@ -0,0 +1,24 @@
+package entities;
+
+public abstract class ElementoMultimediale {
+    private String titolo;
+
+    //    costruttore
+    public ElementoMultimediale(String titolo) {
+        this.titolo = titolo;
+    }
+
+
+//    getter
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+//    setter
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+}
